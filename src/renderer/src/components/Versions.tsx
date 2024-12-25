cbe1f2cdc0150f30b5a962fbe09ad7fb730de724
himu62 <zero.256dic@gmail.com>
2024-12-25 23:50:56 +0900

    nextjs ssr tsuraingo

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -9,7 +9,7 @@ function Versions(): React.ReactElement {
       <li className="electron-version">Electron v{versions.electron}</li>
       <li className="chrome-version">Chromium v{versions.chrome}</li>
       <li className="node-version">Node v{versions.node}</li>
-      <li className="node-version">React v{packageVersions.react}</li>
+      <li>React v{packageVersions.react}</li>
     </ul>
   );
 }
