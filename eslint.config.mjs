07a7e35b98c033365b59e5df18337ae506e218ac
himu62 <zero.256dic@gmail.com>
2024-12-25 18:54:57 +0900

    comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,3 @@
-// module.exports = {
-//   extends: [
-//     "eslint:recommended",
-//     "plugin:react/recommended",
-//     "plugin:react/jsx-runtime",
-//     "@electron-toolkit/eslint-config-ts/recommended",
-//     "@electron-toolkit/eslint-config-prettier",
-//   ],
-//   configs: {
-//     recommended
-//   },
-//   ignorePatterns: ["node_modules", "dist", "out", ".gitignore"],
-// };
-
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
