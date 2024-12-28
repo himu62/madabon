2d218dec416640417fa4eb68542d0052d1293125
himu62 <zero.256dic@gmail.com>
2024-12-28 17:55:46 +0900

    api

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Next, Req, Res } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { postgraphileMiddleware } from "./middleware/postgraphile.middleware";
+import { PostGraphileResponseNode } from "postgraphile";
+import { IncomingMessage } from "http";
+import { ServerResponse } from "http";
 
 @Controller()
 export class AppController {
@@ -8,5 +12,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller("/")
+export class PostGraphileController {
+  @Get(postgraphileMiddleware.graphiqlRoute)
+  graphiql(
+    @Req() request: Request extends IncomingMessage ? Request : IncomingMessage,
+    @Res()
+    response: Response extends ServerResponse ? Response : ServerResponse,
+    @Next() next: (e?: "route" | Error) => void,
+  ) {
+    postgraphileMiddleware.graphiqlRouteHandler(
+      new PostGraphileResponseNode(request, response, next),
+    );
   }
 }
---
+++ apps/api/src/middleware/postgraphile.middleware.ts
@@ -0,0 +1,44 @@
+import { postgraphile, PostGraphileOptions } from "postgraphile";
+import PgSimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
+import { config, Config } from "src/config";
+
+const databaseUrl = (c: Config) => {
+  return `postgres://${c.database.username}:${c.database.password}@${c.database.host}:${c.database.port}/${c.database.database}`;
+};
+
+const devOptions: PostGraphileOptions = {
+  subscriptions: true,
+  watchPg: true,
+  dynamicJson: true,
+  setofFunctionsContainNulls: false,
+  ignoreRBAC: false,
+  showErrorStack: "json",
+  extendedErrors: ["hint", "detail", "errcode"],
+  appendPlugins: [PgSimplifyInflectorPlugin],
+  exportGqlSchemaPath: "schema.graphql",
+  graphiql: true,
+  enhanceGraphiql: true,
+  allowExplain: true,
+  enableQueryBatching: true,
+  legacyRelations: "omit",
+};
+
+const prodOptions: PostGraphileOptions = {
+  subscriptions: true,
+  retryOnInitFail: true,
+  dynamicJson: true,
+  setofFunctionsContainNulls: false,
+  ignoreRBAC: false,
+  extendedErrors: ["errcode"],
+  appendPlugins: [PgSimplifyInflectorPlugin],
+  graphiql: false,
+  enableQueryBatching: true,
+  disableQueryLog: true,
+  legacyRelations: "omit",
+};
+
+export const postgraphileMiddleware = postgraphile(
+  databaseUrl(config),
+  "public",
+  process.env.NODE_ENV === "production" ? prodOptions : devOptions,
+);
